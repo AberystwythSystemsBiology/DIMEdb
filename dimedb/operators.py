3fc0a257c8dda203b54fb32940751ec73a01cd98
KeironO <keiron.oshea@gmail.com>
2017-04-13 13:21:40 +0100

    Raw queries added

--- dimedb/operators.py
+++ dimedb/operators.py
@@ -1,4 +1,5 @@
 from flask_mongorest.operators import Operator
+import documents
 
 '''
     Create an dictonary containing the url-suitable URLs?
@@ -15,24 +16,18 @@ class AdductPpm(Operator):
     def calculate_ppm(self, mz, ppm):
         return abs(float(mz) * (float(ppm) * 0.0001))
 
-
-    def prepare_queryset_kwargs(self, field, value, negate):
-        ionisation, mz, ppm, type = value.split(",")
-
-        self.ionisation = ionisation
-        self.mz = mz
-        self.ppm = ppm
-        self.type = type
-
-
-        difference = self.calculate_ppm(mz, ppm)
-
-        return {
-            "adducts__"+ionisation+"__match": {"accurate_mass__gt": float(mz) - difference,
-                "accurate_mass__lt": float(mz) + difference, "type" : "[M-H]1-"},
-
-        }
-
     def apply(self, queryset, field, value, negate=False):
         kwargs = self.prepare_queryset_kwargs(field, value, negate)
-        return queryset.filter(**kwargs).fields(name=1, accruate_mass=1, adducts= 1).only("adducts."+self.ionisation)
+
+        ionisation, mz, ppm = value.split(",")
+
+        difference = self.calculate_ppm(mz, ppm)
+        print difference
+        return documents.MetaboliteFull.objects(__raw__=(
+            {'adducts.'+ionisation: {
+                '$elemMatch': {
+                    'accurate_mass': {'$gt': float(mz) - difference,
+                                      '$lt': float(mz) + difference}
+                }
+            }}
+        )).only("adducts."+ionisation)
