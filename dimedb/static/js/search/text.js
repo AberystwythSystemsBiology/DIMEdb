9ac67fbf00d026fc82aea34a81e60597b421d8b1
Keiron O'Shea <keiron.oshea@gmail.com>
2017-07-25 18:30:26 +0100

    Added much

---
+++ dimedb/static/js/search/text.js
@@ -0,0 +1,117 @@
+function text_to_null(text) {
+    if (text == "") {
+        return null
+    }
+    else {
+        return $.trim(text)
+    }
+}
+
+function get_values() {
+    return {
+        "Name" : text_to_null($("#name").val()),
+        "Molecular Formula" : text_to_null($("#molecular_formula").val()),
+        "Molecular Weight" : text_to_null($("#molecular_weight").val()),
+        "External Source" : text_to_null($("#source_id").val()),
+        "InChI" : text_to_null($("#inchi").val()),
+        "InChI Key" : text_to_null($("#inchikey").val()),
+        "SMILES" : text_to_null($("#smiles").val())
+    }
+}
+
+function generate_regex(s) {
+    return '{"$regex":"^(?i)'+s+'.*"}';
+}
+
+function checkProperties(obj) {
+    for (var key in obj) {
+        if (obj[key] !== null && obj[key] != "")
+            return false;
+    }
+    return true;
+}
+
+function generate_api_url(values) {
+    var api_url = getBaseURL()+'api/metabolites/?where={';
+    /*
+    if (values["Name"] != null) {
+        console.log(values["Name"]);
+        api_url += '"$or" : [';
+        api_url += '{"Identification Information.Name" :';
+        api_url += generate_regex(values["Name"]) + "},";
+        api_url += '{"Identification Information.IUPAC Name" :';
+        api_url += generate_regex(values["Name"]) + "},";
+        api_url += '{"Identification Information.Systematic Name" :';
+        api_url += generate_regex(values["Name"]) + "},";
+        api_url += '{"Identification Information.Synonyms" : {"$in" : [';
+        api_url += generate_regex(values["Name"]) + "]}}";
+        api_url += "]"
+    }
+    */
+
+    if (values["Molecular Formula"] != null) {
+        api_url += '"Identification Information.Molecular Formula" : ';
+        api_url += generate_regex(values["Molecular Formula"]);
+    }
+
+    api_url += "}";
+
+    console.log(api_url);
+    return api_url;
+
+}
+
+function results_table(api_url) {
+    $('#search_results').DataTable({
+            "destroy": true,
+            "searching": false,
+            "lengthChange": false,
+            "pageLength": 5,
+            "ajax": {
+                "url": encodeURI(api_url),
+                "dataSrc": "_items"
+            },
+            "columns": [
+                {
+                    "title": "Metabolite Name",
+                    "width": "40%",
+                    "data": "Identification Information.Name",
+                    "render": function (data, type, row) {
+                        return "<a href='" + getBaseURL() + "view/" + row._id + "' target='_blank'>" + data + "</a>"
+                    }
+                },
+                {
+                    "title": "Molecular Formula",
+                    "width": "10%",
+                    "className": "dt-center",
+                    "data": "Identification Information.Molecular Formula",
+                    "render": function (data, type, row) {
+                        return data.replace(/([0-9]+)/g, '<sub>$1</sub>');
+                    }
+                },
+                {
+                    "title": "Molecular Weight (g/mol)",
+                    "data": "Physiochemical Properties",
+                    "className": "dt-center",
+                    "width": "10%",
+                    "render": function (data, type, row) {
+                        return data["Molecular Weight"].toFixed(3);
+                    }
+                }
+            ],
+        });
+}
+
+
+$("#submit_search").click(function () {
+    var values = get_values();
+    console.log(values);
+    if (checkProperties(values) == false) {
+        var api_url = generate_api_url(values);
+        results_table(api_url);
+    }
+});
+
+$(document).ready(function () {
+
+});
