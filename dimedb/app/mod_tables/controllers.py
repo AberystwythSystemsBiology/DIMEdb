14c545699207d103d63ae804f69c48dc8b1b368c
Keiron O'Shea <keiron.oshea@gmail.com>
2017-08-09 16:55:01 +0100

    More changes

--- dimedb/app/mod_tables/controllers.py
+++ dimedb/app/mod_tables/controllers.py
@@ -2,9 +2,20 @@ from flask import Blueprint, request, render_template, flash, g, session, redire
 from app import app, db
 
 from app.mod_tables.models import MetaboliteTables, Metabolite
+from app.mod_auth.models import User
 
 tables = Blueprint("tables", __name__)
 
 @app.route("/tables")
 def tables_home():
-    return render_template("tables/index.html")
+    public_tables = MetaboliteTables.query.filter(MetaboliteTables.public == True).all()
+
+    public_tables = User.query.join(MetaboliteTables, User.id == MetaboliteTables.owner_id).add_columns(
+        MetaboliteTables.id,
+        MetaboliteTables.title,
+        MetaboliteTables.creation_date,
+        User.first_name,
+        User.last_name
+    ).filter(MetaboliteTables.public == True).all()
+
+    return render_template("tables/index.html", public_tables=public_tables)
