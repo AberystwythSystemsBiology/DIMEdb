8f7b8656a74e8c6abd9006020b5324d44ceaf231
Keiron O'Shea <keiron.oshea@gmail.com>
2017-08-09 14:24:34 +0100

    Added login screen

--- dimedb/app/mod_auth/controllers.py
+++ dimedb/app/mod_auth/controllers.py
@@ -6,4 +6,41 @@ from flask.ext.login import login_user, logout_user, current_user, login_require
 
 from app.mod_auth.models import User
 
-authentication = Blueprint("auth", __name__, url_prefix="/auth")
+authentication = Blueprint("authentication", __name__)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(user_name=form.user_name.data).first()
+        if user and user.is_correct_password(form.password.data):
+            if user.confirmed == False:
+                flash("Still waiting for account approval")
+            elif user.banned == True:
+                flash("Your account has been banned")
+            elif user.disabled == True:
+                flash("Your account has been disabled")
+            else:
+                login_user(user)
+                flash("Login successful")
+                return redirect(url_for("index"))
+        else:
+            flash("Incorrect email or password given")
+    return render_template("auth/login.html", form=form)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    pass
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+@app.before_request
+def before_request():
+    g.user = current_user
